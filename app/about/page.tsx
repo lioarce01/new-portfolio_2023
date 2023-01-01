a8faf382d2f2c6d1adda48ea3714a7b2ecb8838e
lioarce01 <lioarce1@gmail.com>
2023-01-01 06:19:05 -0300

    added mobile sidebar

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,11 +5,11 @@ import { AiOutlineMail, AiFillLinkedin, AiFillGithub } from "react-icons/ai"
 const About: React.FC = () => {
   return (
     <div className="flex items-center justify-center w-full h-full min-h-screen bg-black">
-			<div className="z-10 flex flex-col justify-center w-full text-white top-28 md:w-[50vw] lg:w-[40vw] px-6">
-				<div className="flex flex-col items-center text-[rgb(230,230,230)] content-center self-center justify-center p-2 text-2xl font-bold text-center bg-transparent rounded-md backdrop-blur-sm">
+			<div className="z-10 flex flex-col justify-center w-full text-white pt-20 md:w-[50vw] lg:w-[40vw] px-6">
+				<div className="flex flex-col items-center text-[rgb(220,220,220)] content-center self-center bg-[rgb(31,31,31)] justify-center p-2 text-2xl font-bold text-center bg-transparent rounded-md backdrop-blur-sm">
 					I&apos;m a Software Developer based in Argentina
 				</div>
-				<div className="mt-28">
+				<div className="mt-24">
 					<p className="w-auto text-2xl font-bold text-[rgb(230,230,230)]">Lionel Arce</p>
 					<small className="font-thin text-lg text-[rgb(230,230,230)]">Software Developer</small>
 				</div>
@@ -19,9 +19,9 @@ const About: React.FC = () => {
 					<p className="pt-4 text-justify text-[rgb(230,230,230)]">In addition to my technical skills, I am also a strong problem solver and have excellent communication and collaboration skills. As a software developer, I am always looking to learn and improve my skills, and I am excited to bring my passion and expertise to any project I work on.</p>
 				</div>
 				<div className='flex items-center justify-center w-full gap-6'>
-					<a target="_blank" rel="noopener noreferrer" href="https://github.com/lioarce01" className='my-16 py-2 px-4 rounded-md text-[rgb(230,230,230)] hover:bg-white hover:text-black transition duration-200 w-fit text-lg border border-white flex flex-row items-center gap-1 justify-center'><AiFillGithub/> <span>Github</span></a>
-					<a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/lionel-arce/" className='my-16 py-2 px-4 rounded-md text-[rgb(230,230,230)] hover:bg-white hover:text-black transition duration-200 w-fit text-lg border border-white flex flex-row items-center gap-1 justify-center'><AiFillLinkedin/> <span>Linkedin</span></a>
-					<a target="_blank" rel="noopener noreferrer" href="mailto:lioarce1@gmail.com" className='my-16 py-2 px-4 rounded-md text-[rgb(230,230,230)] hover:bg-white hover:text-black transition duration-200 w-fit text-lg border border-white flex flex-row items-center gap-1 justify-center'><AiOutlineMail/> <span>Email me</span></a>
+					<a target="_blank" rel="noopener noreferrer" href="https://github.com/lioarce01" className='my-16 py-2 text-sm px-2 rounded-md text-[rgb(230,230,230)] hover:bg-white hover:text-black transition duration-200 w-fit border border-white flex flex-row items-center gap-1 justify-center'><AiFillGithub/> <span>Github</span></a>
+					<a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/lionel-arce/" className='my-16 py-2 text-sm px-2 rounded-md text-[rgb(230,230,230)] hover:bg-white hover:text-black transition duration-200 w-fit border border-white flex flex-row items-center gap-1 justify-center'><AiFillLinkedin/> <span>Linkedin</span></a>
+					<a target="_blank" rel="noopener noreferrer" href="mailto:lioarce1@gmail.com" className='my-16 py-2 text-sm px-2 rounded-md text-[rgb(230,230,230)] hover:bg-white hover:text-black transition duration-200 w-fit border border-white flex flex-row items-center gap-1 justify-center'><AiOutlineMail/> <span>Email me</span></a>
 				</div>
 			</div>
 		</div>
--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,10 +1,15 @@
+"use client"
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../assets/foxLogo-dark.png'
-import {RxHamburgerMenu} from "react-icons/rx"
+import { RxHamburgerMenu } from "react-icons/rx"
+import { AiOutlineClose } from "react-icons/ai"
 
 const Navigation: React.FC = () => {
+	const [toggle, setToggle] = useState(false)
+
+	const toggleSidebar= () => setToggle(!toggle)
 
 	const links = [
 		{
@@ -26,30 +31,45 @@ const Navigation: React.FC = () => {
 	]
 	
   return (
-    <div className="fixed z-50 flex items-center justify-center w-full py-4 bg-black">
-			<div className='flex flex-row w-full md:w-[65%] lg:w-[50%]'>
-				<div className='flex flex-row items-center justify-between w-full px-4'>
-					<div className='flex flex-row items-center gap-2'>
-						<Image src={logo} alt="logo" className="w-10 h-10 transition duration-300 hover:rotate-12" />
-						<p className='text-lg font-bold text-white'>Lionel Arce</p>
-					</div>
-					<button className='md:hidden'>
-						<RxHamburgerMenu className='text-white w-7 h-7'/>
-					</button>
-				</div>
-					<ul className="md:flex md:flex-row w-full md:w-[60%] items-center justify-center hidden">
-						{links.map(({ label, route }) => (
-							<li
-								key={route}
-								className="mx-2 text-xs font-bold text-white transition duration-200 rounded-sm hover:text-white hover:bg-[rgb(87,87,87)]">
-								<Link href={route}>
-									<p className="px-4 py-2">{label}</p>	
-								</Link>
-							</li>
-						))}
-					</ul>
-				</div>
+		<>
+		<div className={`${toggle ? "w-full h-[80%] transition duration-200" : "hidden transition duration-200"} z-50 absolute bg-transparent backdrop-blur-sm lg:hidden transition-all duration-200`}>
+			<div className='flex justify-end w-full pt-2 pr-1'>
+				<button className='p-2 m-2 outline-none rounded-full hover:bg-[rgb(46,46,46)] transition duration-200 text-white self-center content-center' onClick={() => toggleSidebar()}><AiOutlineClose className='w-5 h-5'/></button>
 			</div>
+			<ul>
+				<li className="flex flex-col items-center justify-center w-full gap-10 text-white">
+					<Link href="/" className='w-full px-2 py-4 mx-2 text-center hover:bg-[rgba(20,20,20, 0,5)] text-lg font-bold text-[rgb(220,220,220)]' onClick={() => toggleSidebar()}>Home</Link>
+					<Link href="/about" className='w-full px-2 py-4 mx-2 text-center hover:bg-[rgba(20,20,20, 0,5)] text-lg font-bold text-[rgb(220,220,220)]' onClick={() => toggleSidebar()}>About</Link>
+					<Link href="/skills" className='w-full px-2 py-4 mx-2 text-center hover:bg-[rgba(20,20,20, 0,5)] text-lg font-bold text-[rgb(220,220,220)]' onClick={() => toggleSidebar()}>Skills</Link>
+					<Link href="/projects" className='w-full px-2 py-4 mx-2 text-center hover:bg-[rgba(20,20,20, 0,5)] text-lg font-bold text-[rgb(220,220,220)]' onClick={() => toggleSidebar()}>Projects</Link>
+				</li>
+			</ul>
+		</div>
+			<div className="fixed z-40 flex items-center justify-center w-full py-4 bg-black">
+				<div className='flex flex-row w-full md:w-[65%] lg:w-[50%]'>
+					<div className='flex flex-row items-center justify-between w-full px-4'>
+						<div className='flex flex-row items-center gap-2'>
+							<Image src={logo} alt="logo" className="w-10 h-10 transition duration-300 hover:rotate-12" />
+							<p className='text-lg font-bold text-white'>Lionel Arce</p>
+						</div>
+						<button className='md:hidden' onClick={() => toggleSidebar()}>
+							<RxHamburgerMenu className='text-white w-7 h-7'/>
+						</button>
+					</div>
+						<ul className="md:flex md:flex-row w-full md:w-[60%] items-center justify-center hidden">
+							{links.map(({ label, route }) => (
+								<li
+									key={route}
+									className="mx-2 text-xs font-bold text-white transition duration-200 rounded-sm hover:text-white hover:bg-[rgb(87,87,87)]">
+									<Link href={route}>
+										<p className="px-4 py-2">{label}</p>
+									</Link>
+								</li>
+							))}
+						</ul>
+					</div>
+				</div>
+			</>
   );
 };
 
